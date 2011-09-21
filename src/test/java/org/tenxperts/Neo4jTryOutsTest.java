19fb20b71f0b486c39eba77dda208206ad62c21c
Vineeth Varghese <vineeth.varghese@gmail.com>
2011-09-21 11:10:15 +0530

    Added an index

--- src/test/java/org/tenxperts/Neo4jTryOutsTest.java
+++ src/test/java/org/tenxperts/Neo4jTryOutsTest.java
@@ -3,6 +3,8 @@ package org.tenxperts;
 
 import org.junit.*;
 import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexManager;
 import org.neo4j.graphdb.traversal.Evaluators;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.graphdb.traversal.Traverser;
@@ -22,6 +24,8 @@ import static org.tenxperts.Neo4jTryOutsTest.Edges.ROUTES;
  */
 public class Neo4jTryOutsTest {
 
+    public static final String BUSSTOP = "busstop";
+    public static final String NAME = "name";
     GraphDb graphDb;
 
     @Before
@@ -38,19 +42,22 @@ public class Neo4jTryOutsTest {
     private boolean dataInPlace() {
         return graphDb.doInTx(new Work<Boolean>() {
             public Boolean doWork(GraphDatabaseService graphDatabaseService) {
-                /**
-                 * Create a traverser that starts searching from the root node for relationship ROUTES
-                 * only till a depth 1
-                 */
-                TraversalDescription traversal = Traversal.description().
-                        breadthFirst().
-                        relationships(ROUTES, Direction.OUTGOING).
-                        evaluator(atDepth(1));
-                Traverser traverser = traversal.traverse(graphDatabaseService.getReferenceNode());
-                for (Path path : traverser) {
-                    return true;
-                }
-                return false;
+//                /**
+//                 * Create a traverser that starts searching from the root node for relationship ROUTES
+//                 * only till a depth 1
+//                 */
+//                TraversalDescription traversal = Traversal.description().
+//                        breadthFirst().
+//                        relationships(ROUTES, Direction.OUTGOING).
+//                        evaluator(atDepth(1));
+//                Traverser traverser = traversal.traverse(graphDatabaseService.getReferenceNode());
+//                for (Path path : traverser) {
+//                    return true;
+//                }
+//                return false;
+                Index<Node> busstop = graphDatabaseService.index().forNodes(BUSSTOP);
+                Node node = busstop.get(NAME, "Electronic City").getSingle();
+                return node != null;
             }
         });
     }
@@ -61,29 +68,25 @@ public class Neo4jTryOutsTest {
         }
         graphDb.doInTx(new Work<Void>() {
             public Void doWork(GraphDatabaseService graphDatabaseService) {
-                Node ecity = graphDatabaseService.createNode();
-                ecity.setProperty("name", "Electronic City");
+                Index<Node> busstop = graphDatabaseService.index().forNodes(BUSSTOP);
+
+                Node ecity = createAndIndexNode(graphDatabaseService, busstop, "Electronic City");
                 graphDatabaseService.getReferenceNode().createRelationshipTo(ecity, ROUTES);
 
-                Node bommanahalli = graphDatabaseService.createNode();
-                bommanahalli.setProperty("name", "Bommanahalli");
+                Node bommanahalli = createAndIndexNode(graphDatabaseService, busstop, "Bommanahalli");
                 ecity.createRelationshipTo(bommanahalli, LINKED_TO);
 
-                Node silkBoard = graphDatabaseService.createNode();
-                silkBoard.setProperty("name", "Silk Board");
+                Node silkBoard = createAndIndexNode(graphDatabaseService, busstop, "Silk Board");
                 bommanahalli.createRelationshipTo(silkBoard, LINKED_TO);
 
 
-                Node hsr = graphDatabaseService.createNode();
-                hsr.setProperty("name", "HSR");
+                Node hsr = createAndIndexNode(graphDatabaseService, busstop, "HSR");
                 silkBoard.createRelationshipTo(hsr, LINKED_TO);
 
-                Node madiwala = graphDatabaseService.createNode();
-                madiwala.setProperty("name", "Madiwala");
+                Node madiwala = createAndIndexNode(graphDatabaseService, busstop, "Madiwala");
                 silkBoard.createRelationshipTo(madiwala, LINKED_TO);
 
-                Node koramangala = graphDatabaseService.createNode();
-                koramangala.setProperty("name", "Koramangala");
+                Node koramangala = createAndIndexNode(graphDatabaseService, busstop, "Koramangala");
                 madiwala.createRelationshipTo(koramangala, LINKED_TO);
                 hsr.createRelationshipTo(koramangala, LINKED_TO);
                 bommanahalli.createRelationshipTo(koramangala, LINKED_TO);
@@ -91,6 +94,13 @@ public class Neo4jTryOutsTest {
             }
         });
 
+    }
+
+    private Node createAndIndexNode(GraphDatabaseService graphDatabaseService, Index<Node> index, String name) {
+        Node node = graphDatabaseService.createNode();
+        node.setProperty(NAME, name);
+        index.add(node, NAME, name);
+        return node;
     }
 
     @Test
@@ -105,7 +115,7 @@ public class Neo4jTryOutsTest {
                 Traverser traverse = traversalDescription.traverse(graphDatabaseService.getReferenceNode());
                 int pathCount = 1;
                 for (Path path : traverse) {
-                    System.out.println(" ************ Path " + pathCount + " ************" );
+                    System.out.println(" ************ Path " + pathCount + " ************");
                     Iterable<Node> nodes = path.nodes();
                     for (Node node : nodes) {
                         System.out.println(node.getProperty("name", "Root NodeR"));
